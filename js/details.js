d4a63feac2fe6e254e34ede4f1af05cd40c4ca72
hajnalkafenyo <77206669+hajnalkafenyo@users.noreply.github.com>
2024-01-14 18:29:07 +0100

    Refactoring details

--- js/details.js
+++ js/details.js
@@ -16,41 +16,50 @@ async function getData(url) {
 
 let movieData;
 
-async function showDetails() {
-    try {
-        movieData = await getData(fullUrl);
-    } catch (e) {
-        console.error("Error happened when we tried to load the page")
-        console.error("This is the error that happened", e)
-        document.getElementById("loading").style.display = "none";
-        console.error(":(", e);
-        const main = document.getElementById("movie-details");
-        main.innerHTML = ``;
-        const section = document.getElementsByClassName("error-movie")[0];
-        section.style.display = "flex";
-        return
-    }
+function handleError() {
+    console.error("Error happened when we tried to load the page")
+    console.error("This is the error that happened", e)
+    document.getElementById("loading").style.display = "none";
+    console.error(":(", e);
+    const main = document.getElementById("movie-details");
+    main.innerHTML = ``;
+    const section = document.getElementsByClassName("error-movie")[0];
+    section.style.display = "flex";
+}
 
+function handleSuccess() {
+    document.getElementById("movie-details").style.display = "flex";
+    document.getElementById("loading").style.display = "none";
+
+}
+
+function showMovieData(movieData) {
     document.getElementById("movie-title").innerHTML = movieData.title
     document.getElementById("movie-description").innerHTML = movieData.description
     var tags = []
     tags.push(movieData.genre)
     tags = tags.concat(movieData.tags)
     tags = tags.map((x) => "<li>" + x + "</li>")
-
     document.getElementById("movie-tags").innerHTML = tags.join("")
     document.getElementById("movie-rating").innerHTML = movieData.rating
+    document.getElementById("movie-poster").src = movieData.image;
 
     if (movieData.onSale) {
         document.getElementById("movie-price").innerHTML = `On sale for: <s>${movieData.price} kr</s> ${movieData.discountedPrice} kr`;
     } else {
         document.getElementById("movie-price").innerHTML = `Buy for: ${movieData.price} kr`;
     }
+}
 
-    document.getElementById("movie-poster").src = movieData.image;
-    document.getElementById("movie-details").style.display = "flex";
-    document.getElementById("loading").style.display = "none";
-
+async function showDetails() {
+    try {
+        movieData = await getData(fullUrl);
+    } catch (e) {
+        handleError();
+        return
+    }
+    showMovieData(movieData)
+    handleSuccess()
 }
 
 showDetails();
